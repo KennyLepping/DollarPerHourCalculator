45f8bf11d6d3be85212f5d9b60f8d4614ec04b6c
Kenny Lepping <kennylepping@gmail.com>
2021-09-23 07:02:09 -0400

    Deleted unnecessary hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
