67d687b9fb57a2a4a0a239db84e15a1b0c15a4ae
Kenny Lepping <kennylepping@gmail.com>
2021-09-23 01:37:13 -0400

    Styling the form

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
     let resultCalculation =
       hourlyRate * hours +
       hourlyRate * (minutes / 60) +
-      hourlyRate * ((seconds / 10) / 60);
+      hourlyRate * (seconds / 60  / 60);
+
+    setResult(resultCalculation.toFixed(2));
 
     //    let convertToSeconds = (hours * 3600) + (minutes * 60) + (seconds / 10)
     //    console.log(`convertToSeconds: ${convertToSeconds}`)
     //    let ratePerSecond = hourlyRate / 3600
     //    console.log(`ratePerSeconds: ${ratePerSecond}`)
-
-    setResult(resultCalculation.toFixed(2));
     // setResult((convertToSeconds * ratePerSecond).toFixed(2));
   }, [hourlyRate, hours, minutes, seconds, result]);
 
@@ -27,48 +27,37 @@ function App() {
     <>
       <main>
         <section className="calculation-section">
-          <form>
-            <div>
-              Hours
-              <input
-                className="block"
-                type="number"
-                name="hours"
-                defaultValue={hours || ""}
-                onChange={(e) => setHours(e.target.value)}
-              />
-            </div>
-            <div>
-              Minutes
-              <input
-                className="block"
-                type="number"
-                name="minutes"
-                value={minutes || ""}
-                onChange={(e) => setMinutes(e.target.value)}
-              />
-            </div>
-            <div>
-              Seconds
-              <input
-                className="block"
-                type="number"
-                name="seconds"
-                value={seconds || ""}
-                onChange={(e) => setSeconds(e.target.value)}
-              />
-            </div>
-            <div>
-              Hourly Rate
-              <input
-                className="block"
-                type="number"
-                name="hourlyRate"
-                defaultValue={hourlyRate || ""}
-                onChange={(e) => setHourlyRate(e.target.value)}
-              />
-            </div>
-            <div>Result: {result}</div>
+          <form className="form-styling">
+            <label className="input-name">Hours</label>
+            <input
+              className="block number-input"
+              type="number"
+              defaultValue={hours || ""}
+              onChange={(e) => setHours(e.target.value)}
+            />
+            <label className="input-name">Minutes</label>
+            <input
+              className="block number-input"
+              type="number"
+              value={minutes || ""}
+              onChange={(e) => setMinutes(e.target.value)}
+            />
+            <label className="input-name">Seconds</label>
+            <input
+              className="block number-input"
+              type="number"
+              value={seconds || ""}
+              onChange={(e) => setSeconds(e.target.value)}
+            />
+            <hr />
+            <label className="hourly-rate">Hourly Rate</label>
+            <input
+              className="block number-input"
+              type="number"
+              defaultValue={hourlyRate || ""}
+              onChange={(e) => setHourlyRate(e.target.value)}
+            />
+            <div className="result">Result: <span className="green-text">{result}</span></div>
           </form>
         </section>
       </main>
