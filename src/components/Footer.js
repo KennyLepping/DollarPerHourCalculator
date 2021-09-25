bd3e9dcf5151f12b6e61c7a4dbbaa98233998414
Kenny Lepping <kennylepping@gmail.com>
2021-09-25 02:45:33 -0400

    Added Github repository link to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ export default function footer() {
   return (
     <footer>
       <div>
-        Built by <a href="#">Kenny Lepping</a>
+        Built by{" "}
+        <a href="https://github.com/KennyLepping/DollarPerHourCalculator">
+          Kenny Lepping
+        </a>
       </div>
     </footer>
   );
