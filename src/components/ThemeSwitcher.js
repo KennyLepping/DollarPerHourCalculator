6a6034cee2e0d295117a463c76be83a4e67b1467
Kenny Lepping <kennylepping@gmail.com>
2021-09-30 03:00:12 -0400

    Cleaned up switchFontColor function

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -17,23 +17,29 @@ export default function ThemeSwitcher() {
 
   /* The font color was not switching fast enough when adding the dark class
      This function fixes that */
-  const switchFontColor = (color) => {
-    anime({
+  const switchFontColor = (bodyColor, footerColor) => {
+    const colorTL = anime.timeline({
+      duration: 200,
+    });
+    colorTL.add({
       targets: "body",
-      color: color,
-      duration: 100,
+      color: bodyColor,
+    });
+    colorTL.add({
+      targets: "footer a",
+      color: footerColor,
     });
   };
 
   const switchTheme = () => {
     if (darkTheme) {
       body.classList.remove("dark");
-      switchFontColor("rgb(29, 29, 29)");
+      switchFontColor("rgb(29, 29, 29)", "#2432ff");
       setIconImage(darkThemeIcon);
       setDarkTheme(false);
     } else {
       body.classList.add("dark");
-      switchFontColor("rgb(255, 255, 255)");
+      switchFontColor("rgb(255, 255, 255)", "#2b38ff");
       setIconImage(lightThemeIcon);
       setDarkTheme(true);
     }
